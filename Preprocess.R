fdd415e4d955d04b34eaa75a4e6a9fe44bd7fb03
NickyYi <972775099@qq.com>
2016-01-10 23:53:14 +0800

    nom

--- Preprocess.R
+++ Preprocess.R
@@ -1,12 +1,7 @@
----
-title: "Reproducible Research: Peer Assessment 1"
-output: 
-  html_document:
-    keep_md: true
----
+library(dplyr)
 
 
-## Loading and preprocessing the data
+########################  Download and unzip data  ######################
 download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip", "activity.zip") 
 unzip("activity.zip")
 
@@ -18,8 +13,9 @@ summary(Activity_Pre)
 Activity_Pre$date <- as.Date(Activity_Pre$date)
 sapply(Activity_Pre, class)
 
+####################### Make plots of two dimension: day interval  ######################
+#plot-1 total steps in each day
 
-## What is mean total number of steps taken per day?
 MakePlot_1 <- function(dataset, suffix_char, ...){
   dataset <- group_by(dataset, date)
   assign(paste("steps_sum", suffix_char, sep = ""), summarise(dataset, sum(steps, na.rm = TRUE)), pos = ".GlobalEnv")
@@ -28,9 +24,7 @@ MakePlot_1 <- function(dataset, suffix_char, ...){
   assign(paste("steps_median", suffix_char, sep = ""),summarise(dataset, median(steps,na.rm = TRUE)), pos = ".GlobalEnv")
 }
 MakePlot_1(Activity_Pre, "")
-
-
-## What is the average daily activity pattern?
+#plot-2  average steps in each interval
 Activity_Pre <- group_by(Activity_Pre, interval)
 timeseries <- summarise(Activity_Pre, mean(steps, na.rm = T))
 names(timeseries) <- c("interval", "stepsInInterval")
@@ -41,7 +35,10 @@ axis(1, seq(min(timeseries$interval), max(timeseries$interval),   ## [KP] add x 
 ##Get the interval contains the maxim average steps
 timeseries$interval[which(timeseries$stepsInInterval == max(timeseries$stepsInInterval))]
 
-## Imputing missing values
+######################  Imputing missing values  ######################
+##MNAR: missing not at random
+##MCAR: missing completely at random
+
 ############Missing value exploration
 if (!require(mice)) { ##[KP] package look missing values in number
   install.packages("mice")
@@ -73,7 +70,14 @@ for (i in 1:nrow(Activity_Impute)) {
 }
 cat("Total ", count, "NA values were filled.\n\r")
 
-## Are there differences in activity patterns between weekdays and weekends?
+## Timeseries plot between Impute and Original
+par(mfrow = c(2,1))
+MakePlot_1(Activity_Impute, "_Impute", main="Imputed Data", col='red')
+MakePlot_1(Activity_Pre, "", main="Oringinal Dta", col='green')
+
+
+
+## Weekday Weekends
 Activity_Impute$date <- as.Date(Activity_Impute$date)
 
 work_day <- c("星期一","星期二","星期三","星期四","星期五")
@@ -85,9 +89,6 @@ Activity_Impute_week <- transform(Activity_Impute_week, weekOrweeken = factor(we
 Activity_Impute_week <- group_by(Activity_Impute_week, interval, weekOrweeken)
 groups(Activity_Impute_week)
 Activity_Impute_week_mean <- mutate(Activity_Impute_week, step_means = mean(steps))
-png()
-png(filename = 'plot.png',width = 480, height = 480, bg = rgb(0,0,0,alpha = 0))
 xyplot(step_means~interval | weekOrweeken, data = Activity_Impute_week_mean, type='l', layout = c(1,2),ylab = "Number of steps")
-dev.off()
 
 
